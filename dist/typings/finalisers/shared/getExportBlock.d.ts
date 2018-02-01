b6bf08e258c32f1e3525147c041d7387479a8e27
Kelly Selden <kellyselden@gmail.com>
2018-02-01 07:22:08 -0800

    include dist

---
+++ dist/typings/finalisers/shared/getExportBlock.d.ts
@@ -0,0 +1,7 @@
+import { ChunkExports, ChunkDependencies } from '../../Chunk';
+export default function getExportBlock(
+	exports: ChunkExports,
+	dependencies: ChunkDependencies,
+	exportMode: string,
+	mechanism?: string
+): string;
